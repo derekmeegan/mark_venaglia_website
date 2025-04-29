7f15b9e74cd8d8c631d177ea9d71857dfb5e37b2
derekmeegan <derek.j.meegan@gmail.com>
2025-04-29 10:40:34 -0400

    fix ref to pwa icon

--- vite.config.ts
+++ vite.config.ts
@@ -16,20 +16,9 @@ export default defineConfig({
         theme_color: '#f4b305',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'logo.png',
             sizes: '192x192',
             type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
           }
         ]
       }
